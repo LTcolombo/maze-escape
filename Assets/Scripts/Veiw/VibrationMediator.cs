3efe0b7416a7fd3c3a22986ec2e38140854cdf45
Nick Balyanitsa <n.balyanitsa@gmail.com>
2016-05-31 20:49:06 +0100

    Added vibration feedback fixes #35

---
+++ Assets/Scripts/Veiw/VibrationMediator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Model;
+
+namespace View
+{
+	public class VibrationMediator:MonoBehaviour
+	{
+		void Update ()
+		{
+			if (GameModel.Instance ().state == GameModel.STATE_STUCK)
+				Handheld.Vibrate ();
+		}
+	}
+}
+
